fb6043c4ca89a75f088022eabb236ef102f65331
Emilio De Lazzari <emixdemix@gmail.com>
2026-04-19 15:48:06 +0200

    Fixed s3 and sendmail

--- src/server/email/smtp.ts
+++ src/server/email/smtp.ts
@@ -1,31 +1,34 @@
-import { KeyValue } from '../interfaces'
-import { getTemplate } from '../db/database'
-import nodemailer from 'nodemailer'
+import { KeyValue } from "../interfaces";
+import { getTemplate } from "../db/database";
+import nodemailer from "nodemailer";
+import { SESv2Client, SendEmailCommand } from "@aws-sdk/client-sesv2";
 
 export const sendEmailThirdParty = async (data: KeyValue) => {
-   const transporter = nodemailer.createTransport({
-      host: "email-smtp.eu-west-1.amazonaws.com",
-      port: 587,
-      secure: false,
-      auth: {
-         user: "AKIAYPTG6O5OGX4UPPNN",
-         pass: process.env.SES_PASSWORD,
-      },
-   })
+  try {
+    const sesClient = new SESv2Client({ region: "eu-central-1" });
 
-   const record = await getTemplate(data.templateId)
-   let text = record ? record.html : ''
-   Object.keys(data.dynamic_template_data).forEach(key => {
-      text = text.replaceAll(`{{${key}}}`, data.dynamic_template_data[key])
-   })
+    const transporter = nodemailer.createTransport({
+      SES: { sesClient, SendEmailCommand },
+    });
 
-   const info = await transporter.sendMail({
-      from: 'support@artinventory.de',
+    const record = await getTemplate(data.templateId);
+    let text = record ? record.html : "";
+    Object.keys(data.dynamic_template_data).forEach((key) => {
+      text = text.replaceAll(`{{${key}}}`, data.dynamic_template_data[key]);
+    });
+
+    const info = await transporter.sendMail({
+      from: "support@artinventory.de",
       to: data.to,
       subject: data.dynamic_template_data.subject,
       text: text,
-      html: text
-   })
+      html: text,
+    });
 
-   return info
-}
+    console.log("INFO", info);
+
+    return info;
+  } catch (e) {
+    console.log("Error:", e, process.env.SES_PASSWORD);
+  }
+};
